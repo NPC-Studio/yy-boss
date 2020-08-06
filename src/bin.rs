823afc325dff8da9015eecab955f17f48f2b4549
jack <jjspira@gmail.com>
2020-08-06 00:54:34 -0700

    Init

--- src/bin.rs
+++ src/bin.rs
@@ -1,1 +1,19 @@
-pub fn main() {}
+mod cli {
+    mod input;
+    pub use input::*;
+}
+
+pub use yy_boss::*;
+
+pub fn main() {
+    let input = cli::parse_inputs();
+
+    match YypBoss::new(&input.yyp_path) {
+        Ok(_) => {
+            println!("Nice job");
+        }
+        Err(e) => {
+            eprintln!("!!Error!!: Could not load Yyp...{:?}", e);
+        }
+    }
+}
