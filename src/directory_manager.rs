f79ffdef1910fedef3bccb7d2ef893d7ca0d9047
Jonathan Spira <jjspira@gmail.com>
2023-09-12 22:32:18 -0400

    removing the boss directory forever

--- src/directory_manager.rs
+++ src/directory_manager.rs
@@ -1,5 +1,4 @@
-use super::{errors::StartupError, utils};
-use crate::FileSerializationError;
+use super::errors::StartupError;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug, PartialEq, Eq, Default)]
@@ -13,17 +12,9 @@ pub struct DirectoryManager {
     ///
     /// The yyp path is `root_directory.join(yyp_name)`.
     yyp: PathBuf,
-
-    /// The path to the boss directory, which is within the `root_directory`. We cache it here
-    /// for simplicity.
-    ///
-    /// The boss directory is `root_directory.join(".boss")`.
-    boss_directory: PathBuf,
 }
 
 impl DirectoryManager {
-    const YYBOSS_DIR: &'static str = ".boss";
-
     /// Creates a new Directory manager and initializes the boss directory, if it doesn't exist.
     pub(crate) fn new(yyp: &Path) -> Result<Self, StartupError> {
         let root_directory = yyp
@@ -34,15 +25,7 @@ impl DirectoryManager {
             })?
             .to_owned();
 
-        let boss_directory = root_directory.join(Path::new(Self::YYBOSS_DIR));
-
-        if boss_directory.exists() == false {
-            std::fs::create_dir(&boss_directory)
-                .map_err(|e| StartupError::BossDirectory(e.to_string()))?;
-        }
-
         let output = DirectoryManager {
-            boss_directory,
             root_directory,
             yyp: yyp.to_owned(),
         };
@@ -60,23 +43,8 @@ impl DirectoryManager {
         &self.yyp
     }
 
-    /// Creates a path into the boss directory.
-    pub fn boss_file(&self, relative_path: &Path) -> PathBuf {
-        self.boss_directory.join(relative_path)
-    }
-
     /// Creates a path within the root directory, probably to a resource.
     pub fn resource_file(&self, relative_path: &Path) -> PathBuf {
         self.root_directory.join(relative_path)
-    }
-
-    /// Saves a file within the boss directory, as a compound operation.
-    pub fn serialize_boss_file(
-        &self,
-        relative_path: &Path,
-        data: String,
-    ) -> Result<(), utils::FileSerializationError> {
-        std::fs::write(self.boss_file(relative_path), data)
-            .map_err(|e| FileSerializationError::Io(e.to_string()))
     }
 }
